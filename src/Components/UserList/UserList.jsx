6d1341290ada0482431da4efe2a4b6e96750b365
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2023-12-02 02:39:48 +0600

    day-11-FriendRequest

--- src/Components/UserList/UserList.jsx
+++ src/Components/UserList/UserList.jsx
@@ -2,12 +2,13 @@ import { BsThreeDotsVertical } from '@react-icons/all-files/bs/BsThreeDotsVertic
 import { FaPlus } from '@react-icons/all-files/fa/FaPlus'
 import React from 'react'
 import userListImg from '../../../public/images/userList.png'
-import { getDatabase, ref, child, get, onValue, set } from "firebase/database";
+import { getDatabase, ref, child, get, onValue, set, push } from "firebase/database";
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 const UserList = () => {
     const [userDataList, setUserDataList] = useState([])
+    const [sendFriendRequestData , setSendFriendRequestData] = useState([])
     const db = getDatabase();
     const userData = useSelector(state => state.userLoginInfo.userInfo)
     // console.log(userData)
@@ -22,7 +23,7 @@ const UserList = () => {
                 if (userData.uid != userItem.key){
                     arr.push({...userItem.val(), userId : userItem.key });
                 }
-                // console.log(userItem.val())
+                // console.log(userItem.key)
             })
             
             setUserDataList(arr)
@@ -32,7 +33,7 @@ const UserList = () => {
     
     const sendFriendRequest = (item) =>{
         // console.log(item);
-        set(ref(db, 'friendRequesr/'), {
+        set(push(ref(db, 'friendRequest/')), {
             senderName: userData.displayName,
             senderUid: userData.uid,
             rechiverName : item.username ,
@@ -41,6 +42,19 @@ const UserList = () => {
           });
         //   console.log(item)
     }
+    useEffect(() =>{
+        const userRef = ref(db, 'friendRequest/');
+        onValue(userRef, (snapshot) => {
+            var arr = []
+            snapshot.forEach((friendRequestData) =>{
+                // arr.push({...friendRequestData.val()})
+                arr.push(friendRequestData.val().rechiverUid + friendRequestData.val().senderUid);
+            })
+            setSendFriendRequestData(arr) 
+            // console.log(sendFriendRequestData);
+          });
+        },[])
+        
     return (
         <div className='p-[22px] shadow-sm rounded-[20px] ml-[20px] w-[330px] h-[432px] mt-[20px]'>
             <div className="UserList relative  flex justify-between items-center ">
@@ -59,7 +73,11 @@ const UserList = () => {
                                 <h1 className='userListName text-[16px] font-semibold'>{item.username}</h1>
                                 <p className="userListMessage text-[14px] text-[#797979]">Today, 2:31pm</p>
                             </div>
-                            <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold"><FaPlus></FaPlus></button>
+                            {
+                                sendFriendRequestData.includes(userData.uid+item.userId) || sendFriendRequestData.includes(item.userId+userData.uid) ? <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Pending</button> : 
+                                <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold"><FaPlus></FaPlus></button>
+                            }
+                            
                         </div>
                 ))
                 
