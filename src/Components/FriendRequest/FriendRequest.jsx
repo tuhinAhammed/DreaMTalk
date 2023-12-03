4a98dbcd3d12279fba475ff57f37dd0eeb9cd3ae
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2023-12-03 20:26:31 +0600

    Day-12-ImportantClass-RequestList to FriendList

--- src/Components/FriendRequest/FriendRequest.jsx
+++ src/Components/FriendRequest/FriendRequest.jsx
@@ -1,48 +1,67 @@
 import { BsThreeDotsVertical } from '@react-icons/all-files/bs/BsThreeDotsVertical'
 import React from 'react'
 import friendRequestImg from '../../../public/images/friendRequest.png'
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 const FriendRequest = () => {
-  return (
-    <div className='p-[22px] shadow-sm rounded-[20px] ml-[20px] w-[330px] mt-[20px]'>
-        <div className="GroupList relative  flex justify-between items-center ">
-            <h1 className='text-[20px] font-bold'>Friend  Request</h1>               
-            <BsThreeDotsVertical className='text-[18px] absolute right-[0]' ></BsThreeDotsVertical>
-        </div>
-        <div className="innerList  ">
-            <div className='flex justify-between items-center border-b-[1px] border-[#BFBFBF] py-[12px]'>
-                    <div className="groupImage ">
-                        <img src={friendRequestImg} className="w-[95%]" alt="" />
-                    </div>
-                    <div className="GroupTitle">
-                        <h1 className='groupName text-[16px] font-semibold'>Kiran</h1>
-                        <p className="groupMeaase text-[14px] text-[#797979]">Hi....!</p>
-                    </div>
-                    <button className="join px-[15px] py-[2px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Accept</button>
-            </div>
-            <div className='flex justify-between items-center border-b-[1px] border-[#BFBFBF] py-[12px]'>
-                    <div className="groupImage ">
-                        <img src={friendRequestImg} className="w-[95%]" alt="" />
-                    </div>
-                    <div className="GroupTitle">
-                        <h1 className='groupName text-[16px] font-semibold'>Kiran</h1>
-                        <p className="groupMeaase text-[14px] text-[#797979]">Hi....!</p>
-                    </div>
-                    <button className="join px-[15px] py-[2px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Accept</button>
-            </div>
-            <div className='flex justify-between items-center border-b-[1px] border-[#BFBFBF] py-[12px]'>
-                    <div className="groupImage ">
-                        <img src={friendRequestImg} className="w-[95%]" alt="" />
-                    </div>
-                    <div className="GroupTitle">
-                        <h1 className='groupName text-[16px] font-semibold'>Kiran</h1>
-                        <p className="groupMeaase text-[14px] text-[#797979]">Hi....!</p>
-                    </div>
-                    <button className="join px-[15px] py-[2px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Accept</button>
-            </div>
+    const data = useSelector(state => state.userLoginInfo.userInfo)
+    // console.log(data);
+    const db = getDatabase();
+    const [requestAcceptList, setRequestAcceptList] = useState([])
 
+    const handleAcceptFriend = (item) =>{
+        console.log(item);
+        set(push(ref(db, 'friends/' )), {
+            ...item
+          }).then(() =>{
+            remove(ref(db, 'friendRequest/' + item.id ))
+            console.log("all is well");
+          })  
+    }
+    useEffect(() => {
+        const requestList = ref(db, 'friendRequest/');
+        onValue(requestList, (snapshot) => {
+            let arr = []
+            snapshot.forEach((recheveRequest) => {
+                console.log(recheveRequest.val());
+                if (recheveRequest.val().rechiverUid == data.uid) {
+                    arr.push({ ...recheveRequest.val() , id: recheveRequest.key })
+                }
+            })
+            setRequestAcceptList(arr)
+        });
+        console.log(requestAcceptList);
+    }, [])
+
+    return (
+        <div className='p-[22px] shadow-sm rounded-[20px] ml-[20px] w-[330px] mt-[20px]'>
+            <div className="GroupList relative  flex justify-between items-center ">
+                <h1 className='text-[20px] font-bold'>Friend  Request</h1>
+                <BsThreeDotsVertical className='text-[18px] absolute right-[0]' ></BsThreeDotsVertical>
+            </div>
+            <div className="innerList">
+
+                {
+                    requestAcceptList.map((item) => (
+                        <div className='flex justify-between items-center border-b-[1px] border-[#BFBFBF] py-[12px]'>
+                            <div className="groupImage ">
+                                <img src={friendRequestImg} className="w-[95%]" alt="" />
+                            </div>
+                            <div className="GroupTitle">
+                                <h1 className='groupName text-[16px] font-semibold'>{item.senderName}</h1>
+                                <p className="groupMeaase text-[14px] text-[#797979]">Hi....!</p>
+                            </div>
+                            <button onClick={() => handleAcceptFriend(item)} className="join px-[15px] py-[2px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Accept</button>
+                        </div>
+                    ))
+                }
+
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default FriendRequest
--- src/Components/UserList/UserList.jsx
+++ src/Components/UserList/UserList.jsx
@@ -9,6 +9,7 @@ import { useSelector } from 'react-redux';
 const UserList = () => {
     const [userDataList, setUserDataList] = useState([])
     const [sendFriendRequestData , setSendFriendRequestData] = useState([])
+    const [friendListData , setFriendListData] = useState([])
     const db = getDatabase();
     const userData = useSelector(state => state.userLoginInfo.userInfo)
     // console.log(userData)
@@ -29,7 +30,7 @@ const UserList = () => {
             setUserDataList(arr)
         });
     }, [])
-    console.log(userDataList);
+    // console.log(userDataList);
     
     const sendFriendRequest = (item) =>{
         // console.log(item);
@@ -38,7 +39,6 @@ const UserList = () => {
             senderUid: userData.uid,
             rechiverName : item.username ,
             rechiverUid : item.userId ,
-
           });
         //   console.log(item)
     }
@@ -54,6 +54,18 @@ const UserList = () => {
             // console.log(sendFriendRequestData);
           });
         },[])
+    useEffect(() =>{
+        const friendList = ref(db, 'friends/');
+        onValue(friendList, (snapshot) => {
+            var arr = []
+            snapshot.forEach((friendRequestData) =>{
+                // arr.push({...friendRequestData.val()})
+                arr.push(friendRequestData.val().rechiverUid + friendRequestData.val().senderUid);
+            })
+            setFriendListData(arr) 
+        });
+        console.log(friendListData);
+    },[])
         
     return (
         <div className='p-[22px] shadow-sm rounded-[20px] ml-[20px] w-[330px] h-[432px] mt-[20px]'>
@@ -74,9 +86,15 @@ const UserList = () => {
                                 <p className="userListMessage text-[14px] text-[#797979]">Today, 2:31pm</p>
                             </div>
                             {
-                                sendFriendRequestData.includes(userData.uid+item.userId) || sendFriendRequestData.includes(item.userId+userData.uid) ? <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold">Pending</button> : 
-                                <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold"><FaPlus></FaPlus></button>
+                                friendListData.includes(userData.uid+item.userId) || friendListData.includes(item.userId+userData.uid) 
+                                ? 
+                                <button  className="join px-[8px] py-[4px] bg-primary rounded-[5px] text-[#fff] text-[18px] font-semibold cursor-none">Friend</button>
+                                 : 
+
+                                sendFriendRequestData.includes(userData.uid+item.userId) || sendFriendRequestData.includes(item.userId+userData.uid) ? <button  className="join px-[8px] py-[4px] bg-primary rounded-[5px] text-[#fff] text-[16px] font-semibold cursor-none">Pending</button> : 
+                                <button onClick={() => sendFriendRequest(item)} className="join px-[8px] py-[8px] bg-primary rounded-[5px] text-[#fff] text-[16px] font-semibold"><FaPlus></FaPlus></button>
                             }
+                            
                             
                         </div>
                 ))
